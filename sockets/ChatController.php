90d2468d8d59ebab1ef5d4633ac28808984dab29
Yar0Skr <yar.skrypnyk@gmail.com>
2021-07-02 14:42:27 +0300

    working sockets

--- sockets/ChatController.php
+++ sockets/ChatController.php
@@ -5,16 +5,27 @@ use blackbes\yiisockets\BaseController;
 
 class ChatController extends BaseController
 {
-    public function actionStartChat() {
+
+    public function actionSubscribeChat() {
         $user = Users::findOne($this->client_id);
         $chatId = $this->getData('chat_id');
-        $this->sendToGroup('chats/start', ['user_id' => $user->id], 'chat-' . $chatId);
+        $this->addToGroup($this->conn,'chat-'.$chatId);
+        $this->sendToGroup('status', ['user_id' => $user->id, "status"=>"Successfully joined chat"], 'chat-' . $chatId);
     }
 
-    public function actionStopChat() {
+
+    public function actionUnsubscribeChat() {
         $user = Users::findOne($this->client_id);
         $chatId = $this->getData('chat_id');
-        $this->sendToGroup('chats/stop', ['user_id' => $user->id], 'chat-' . $chatId);
+        $this->removeFromGroup($this->conn,'chat-'.$chatId);
+        $this->sendToGroup('status', ['user_id' => $user->id, "status"=>"Successfully left chat"], 'chat-' . $chatId);
+    }
+
+    public function actionSend() {
+        $user = Users::findOne($this->client_id);
+        $text = $this->getData('text');
+        $chatId = $this->getData('chat_id');
+        $this->sendToGroup('new-message', ['user_id' => $user->id, 'text' => $text, 'chat_id'=>$chatId], 'chat-' . $chatId);
     }
 
     public function actionTest() {
