4611b5a8aee1ef0ec45d3dc7457128be3a402d4c
Yar0Skr <yar.skrypnyk@gmail.com>
2021-07-05 15:17:56 +0300

    add messages to database

--- sockets/ChatController.php
+++ sockets/ChatController.php
@@ -1,5 +1,7 @@
 <?php
 namespace app\sockets;
+use app\models\ChatMembers;
+use app\models\ChatMessages;
 use app\models\Users;
 use blackbes\yiisockets\BaseController;
 
@@ -25,7 +27,17 @@ class ChatController extends BaseController
         $user = Users::findOne($this->client_id);
         $text = $this->getData('text');
         $chatId = $this->getData('chat_id');
-        $this->sendToGroup('new-message', ['user_id' => $user->id, 'text' => $text, 'chat_id'=>$chatId], 'chat-' . $chatId);
+
+        $chatMember = ChatMembers::findOne(['user_id'=>$user->id, 'chat_id'=>$chatId]);
+        $model = new ChatMessages();
+        $model->message = $text;
+        $model->member_id = $chatMember->id;
+        if ($model->save()){
+            $this->sendToGroup('new-message', ['user_id' => $user->id, 'text' => $text, 'chat_id'=>$chatId], 'chat-' . $chatId);
+        } else {
+            $this->sendToGroup('error', ['error' => "Problem saving message"], 'chat-' . $chatId);
+
+        }
     }
 
     public function actionTest() {
